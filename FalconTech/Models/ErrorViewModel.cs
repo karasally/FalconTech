b4286a7755b0355907bdd72f93621c389b2834a5
Kara Sally <karasally06@gmail.com>
2023-11-29 16:07:30 +0000

    Add project files.

---
+++ FalconTech/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FalconTech.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
